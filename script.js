f52952453e1ad43ff9c1645be0c93bf9a303bcac
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 20:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-// complete this js code
-function Person(name, age) {}
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
 
-function Employee(name, age, jobTitle) {}
+const express = require('express');
+const path = require('path');
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
